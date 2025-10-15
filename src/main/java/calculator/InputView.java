1df42d462b73a8cd84d462705cca6314aa20dfd9
Jiihyun <cwlgusc@gmail.com>
2025-10-15 19:26:52 +0900

    feat(view): 문자열 입력 받는 기능 구현
    값을 입력받아 반환합니다.
    만약 값이 비었다면 0을 반환합니다.

---
+++ src/main/java/calculator/InputView.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String DEFAULT_VALUE = "0";
+
+    public String readInput() {
+        System.out.println(INPUT_MESSAGE);
+
+        String input = readLine();
+        if (input.isBlank()) {
+            return DEFAULT_VALUE;
+        }
+        return input;
+    }
+
+    private String readLine() {
+        return Console.readLine().strip();
+    }
+}
