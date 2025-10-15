5d26062802fdf9ae1c336e6bfae0dddb25afdda9
Jiihyun <cwlgusc@gmail.com>
2025-10-15 23:59:17 +0900

    feat(view): 덧셈 결과 출력 기능 구현

---
+++ src/main/java/calculator/OutputView.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class OutputView {
+
+    private static final String RESULT_MESSAGE = "결과 : ";
+
+    public void showResult(long sum) {
+        System.out.println(RESULT_MESSAGE + sum);
+    }
+}
