a66dd2bc32e8f8bde7cf4ec84ca39b25404a1482
Jiihyun <cwlgusc@gmail.com>
2025-10-15 23:58:29 +0900

    feat(model): 숫자들 덧셈 기능 구현
    long 범위 초과시 예외 처리 하였습니다.

--- src/main/java/calculator/Numbers.java
+++ src/main/java/calculator/Numbers.java
@@ -6,32 +6,41 @@ public class Numbers {
 
     private static final int POSITIVE_NUMBER_THRESHOLD = 1;
 
-    private final List<Integer> numbers;
+    private final List<Long> numbers;
 
-    public Numbers(List<Integer> numbers) {
+    public Numbers(List<Long> numbers) {
         validateSize(numbers);
         validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validateSize(List<Integer> numbers) {
+    private void validateSize(List<Long> numbers) {
         if (numbers.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 숫자가 존재하지 않습니다.");
         }
     }
 
-    private void validateRange(List<Integer> numbers) {
+    private void validateRange(List<Long> numbers) {
         if (isOutOfRange(numbers)) {
             throw new IllegalArgumentException("[ERROR] 숫자는 양수만 가능합니다.");
         }
     }
 
-    private boolean isOutOfRange(List<Integer> numbers) {
+    private boolean isOutOfRange(List<Long> numbers) {
         return numbers.stream()
                 .anyMatch(this::hasNonPositiveNumber);
     }
 
-    private boolean hasNonPositiveNumber(Integer number) {
+    private boolean hasNonPositiveNumber(Long number) {
         return number < POSITIVE_NUMBER_THRESHOLD;
+    }
+
+    public long sum() {
+        try {
+            return numbers.stream()
+                    .reduce(0L, Math::addExact);
+        } catch (ArithmeticException e) {
+            throw new IllegalArgumentException("[ERROR] 덧셈 가능한 범위를 초과하였습니다");
+        }
     }
 }
