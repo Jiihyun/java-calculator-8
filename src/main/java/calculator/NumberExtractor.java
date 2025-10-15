0a53652f5166ae511005a5e157aa733e2465aa8d
Jiihyun <cwlgusc@gmail.com>
2025-10-15 23:45:48 +0900

    refactor(model): 객체 생성 시 중복되는 코드 메서드로 분리
    Numbers 객체 생성하는 메서드 분리 및 & number 타입 Long으로 변환하였습니다.

--- src/main/java/calculator/NumberExtractor.java
+++ src/main/java/calculator/NumberExtractor.java
@@ -10,28 +10,25 @@ public class NumberExtractor {
     private static final String DELIMITER_COLON = ":";
 
     public Numbers extract(String value) {
-        try {
-            List<Integer> numbers = Arrays.stream(value.split(DELIMITER_COMMA + "|" + DELIMITER_COLON))
-                    .map(Integer::parseInt)
-                    .toList();
-            return new Numbers(numbers);
-        } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException("[ERROR] 구분자와 양수외의 문자가 포함되어 있습니다.");
-        }
+        String regex = DELIMITER_COMMA + "|" + DELIMITER_COLON;
+        return createNumbers(value, regex);
     }
 
     public Numbers extractWith(String customDelimiter, String value) {
+        int endIndex = value.indexOf("\\n");
+        String parsedValue = value.substring(endIndex + 2);
+        String regex = DELIMITER_COMMA + "|" + DELIMITER_COLON + "|" + Pattern.quote(customDelimiter);
+        return createNumbers(parsedValue, regex);
+    }
+
+    private static Numbers createNumbers(String value, String regex) {
         try {
-            int endIndex = value.indexOf("\\n");
-            String parsedValue = value.substring(endIndex + 2);
-            String regex = DELIMITER_COMMA + "|" + DELIMITER_COLON + "|" + Pattern.quote(customDelimiter);
-            String[] split = parsedValue.split(regex);
-            List<Integer> numbers = Arrays.stream(split)
-                    .map(Integer::parseInt)
+            List<Long> numbers = Arrays.stream(value.split(regex))
+                    .map(Long::parseLong)
                     .toList();
             return new Numbers(numbers);
         } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException("[ERROR] 구분자와 양수외의 문자가 포함되어 있습니다.");
+            throw new IllegalArgumentException("[ERROR] 인정되지 않는 문자 또는 숫자가 포함되어 있습니다.");
         }
     }
 }
