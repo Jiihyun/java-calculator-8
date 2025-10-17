beb0fb1f6f82861c5bffc8953afff6d6e4626833
Jiihyun <cwlgusc@gmail.com>
2025-10-17 13:34:05 +0900

    refactor: 패키지 구조 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,13 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.Calculator;
+import calculator.domain.DelimiterExtractor;
+import calculator.domain.ExpressionParser;
+import calculator.domain.NumberExtractor;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         CalculatorController calculatorController = new CalculatorController(
--- src/main/java/calculator/domain/ExpressionInfo.java
+++ src/main/java/calculator/domain/ExpressionInfo.java
@@ -1,4 +1,4 @@
-package calculator;
+package calculator.domain;
 
 public record ExpressionInfo(
         String expression,
