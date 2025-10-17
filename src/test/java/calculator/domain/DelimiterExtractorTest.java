4d3ce52ff537679b4bb9f1d658dbfd6797a29b1a
Jiihyun <cwlgusc@gmail.com>
2025-10-17 17:08:38 +0900

    test: DelimiterExtractor 테스트 빈 문자열 케이스 추가

--- src/test/java/calculator/domain/DelimiterExtractorTest.java
+++ src/test/java/calculator/domain/DelimiterExtractorTest.java
@@ -2,6 +2,8 @@ package calculator.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,20 +20,19 @@ class DelimiterExtractorTest {
     @Test
     void 커스텀_구분자를_추출할_수_있다() {
         // given
-        String value = "//;\\n1;2;3";
-        String expectDelimiter = ";";
+        String expression = "//[plus]\\n1[plus]2[plus]3";
+        String expectDelimiter = "[plus]";
         // when
-        String resultDelimiter = delimiterExtractor.extract(value);
+        String resultDelimiter = delimiterExtractor.extract(expression);
         // then
         assertThat(resultDelimiter).isEqualTo(expectDelimiter);
     }
 
-    @Test
-    void 커스텀_구분자가_빈문자열이면_예외를_반환한다() {
-        // given
-        String value = "// \\n1;2;3";
+    @ParameterizedTest
+    @ValueSource(strings = {"//\\n1;2;3", "// \\n1;2;3", "//    \\n1;2;3"})
+    void 커스텀_구분자가_빈문자열이면_예외를_반환한다(String expression) {
         // when & then
-        assertThatThrownBy(() -> delimiterExtractor.extract(value))
+        assertThatThrownBy(() -> delimiterExtractor.extract(expression))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 커스텀 구분자는 빈 문자열일 수 없습니다.");
     }
@@ -39,9 +40,9 @@ class DelimiterExtractorTest {
     @Test
     void 커스텀_구분자에_숫자가_포함되면_예외를_반환한다() {
         // given
-        String value = "//^1^\\n1;2;3";
+        String expression = "//^1^\\n1;2;3";
         // when & then
-        assertThatThrownBy(() -> delimiterExtractor.extract(value))
+        assertThatThrownBy(() -> delimiterExtractor.extract(expression))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 커스텀 구분자는 숫자를 포함할 수 없습니다.");
     }
